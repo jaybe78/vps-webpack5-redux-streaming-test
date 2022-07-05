edd00d8d31dd6415c661be5e3efa5f5a290eaff0
jb <jb.rougeot@lightdash.com>
2022-07-05 10:27:15 +0100

    can't import server if there's svg

--- pages/all.page.tsx
+++ pages/all.page.tsx
@@ -10,21 +10,6 @@ const LazyAbout = LazyPage({ page: "About" });
 function Page() {
   return (
     <>
-      <b>
-        <i>
-          Time elapsed: <TimeElapsed />
-        </i>
-        <Counter />
-      </b>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-      </ul>
-      <hr />
       <React.Suspense
         fallback={<span className="no-result-search">Loading...</span>}
       >
@@ -34,31 +19,5 @@ function Page() {
         </Routes>
       </React.Suspense>
     </>
-  );
-}
-
-function TimeElapsed() {
-  const [count, setCount] = useState(0);
-
-  useEffect(() => {
-    const timeout = setInterval(() => {
-      setCount((c) => c + 1);
-    }, 1000);
-    return () => clearTimeout(timeout);
-  }, []);
-
-  return <>{count}</>;
-}
-
-function Counter() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <button
-      onClick={() => setCount((count) => count + 1)}
-      style={{ marginLeft: 10 }}
-    >
-      Count: <span>{count}</span>
-    </button>
   );
 }
